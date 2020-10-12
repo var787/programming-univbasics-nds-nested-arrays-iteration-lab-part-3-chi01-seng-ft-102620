b17355503bb4f00ee109a0b087876dde40fe2994
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 07:27:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
 
-    if src[row_index][element_index] == String
+    if src[row_index][element_index]==String
       outer_results << src[row_index][element_index]
 
 end
