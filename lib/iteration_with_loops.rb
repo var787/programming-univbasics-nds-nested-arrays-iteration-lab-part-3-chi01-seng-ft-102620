336568cdd168f1984b3e908f00b2f65cdff9df06
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 07:30:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,6 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
       arr<< src[row_index][element_index]
-
-end
   element_index += 1
   end
   row_index += 1
