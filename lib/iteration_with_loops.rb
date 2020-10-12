bc42b29d7a06c0128ecc33cac2174a62828746e2
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 07:30:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,6 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-
-    if src[row_index][element_index]=="F"
       arr<< src[row_index][element_index]
 
 end
