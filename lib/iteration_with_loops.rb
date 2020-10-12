08b60e60e1cc9beef14f7b0d867348b78714d7c1
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 07:28:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,12 @@
 def join_nested_strings(src)
-  outer_results =[]
+  arr=[]
 row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
 
     if src[row_index][element_index]=="F"
-      outer_results << src[row_index][element_index]
+      arr<< src[row_index][element_index]
 
 end
   element_index += 1
