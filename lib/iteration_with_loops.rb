e5d045930fc899cf24c9200ac639a181508c320d
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 07:34:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-        if src[row_index][element_index].class==Integer
+        if src[row_index][element_index].class==String
           arr<< src[row_index][element_index]
 
         end
